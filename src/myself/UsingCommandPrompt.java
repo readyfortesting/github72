403fb5cc7f2cf077178f85199a12d91696a7ec8d
Burak Akyuz <tallboy6077@gmail.com>
2021-12-09 19:42:52 +0300

    first commit

---
+++ src/myself/UsingCommandPrompt.java
@@ -0,0 +1,10 @@
+package myself;
+
+public class UsingCommandPrompt {
+
+	public static void main(String[] args) {
+		System.out.println("Today on 12.09.2021 I did my first command using command prompt");
+
+	}
+
+}
